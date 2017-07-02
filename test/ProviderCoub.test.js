0abff6725d030cb62707d1bedcd3613f0657ddd0
kotborealis <kotborealis@gmail.com>
2017-07-02 18:33:46 +0300

    Updated coub test

--- test/ProviderCoub.test.js
+++ test/ProviderCoub.test.js
@@ -19,7 +19,13 @@ describe('ProviderCoub', () => {
     describe('info', () => {
         it('should return video info', (done) => {
             coub.info('http://coub.com/view/divbg').then(entity => {
-                console.log(entity);
+                expect({ duration: null,
+                    title: 'wheeel',
+                    thumbnail: 'http://s.storage.akamai.coub.com/get/b114/p/coub/simple/cw_image/ba6f7233a15/767324b7e4f6bf5cdf015/med_1474010492_00032.jpg',
+                    url: 'https://coub.com/view/divbg',
+                    type: 'coub',
+                    disableTiming: false,
+                    id: 'divbg' }).to.deep.equal(entity);
                 done();
             }).catch(reason => {
                 throw new Error(reason);
