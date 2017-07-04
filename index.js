b9b2bc537805d3b9d2c4780e844e6a934aa561fa
kotborealis <kotborealis@gmail.com>
2017-07-04 23:14:15 +0300

    Forgot to add soundcloud export :/

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ module.exports = {};
 
 const capitalize = ([first,...rest]) => first.toUpperCase() + rest.join('').toLowerCase();
 
-['coub', 'file', 'twitchStream', 'vimeo', 'youtube', 'youtubeList'].forEach(provider => {
+['coub', 'file', 'twitchStream', 'vimeo', 'youtube', 'youtubeList', 'soundcloud'].forEach(provider => {
     module.exports[provider] = require('./lib/providers/Provider' + capitalize(provider));
 });
