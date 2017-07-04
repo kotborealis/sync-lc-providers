764a896eaec506773fd1d90823df175706e3ea21
kotborealis <kotborealis@gmail.com>
2017-07-04 18:21:45 +0300

    soundcloud list

--- test/ProviderSoundcloud.test.js
+++ test/ProviderSoundcloud.test.js
@@ -17,13 +17,112 @@ describe('ProviderSoundcloud', () => {
     describe('info', () => {
         it('should return video info', (done) => {
             soundcloud.info('https://soundcloud.com/raxxo-1/ng-mare').then(entity => {
-                expect({ duration: 98463,
+                expect({ duration: 98,
                     title: 'NG Mare',
                     thumbnail: 'https://i1.sndcdn.com/artworks-000145582566-ihitj3-large.jpg',
                     url: 'https://soundcloud.com/raxxo-1/ng-mare',
                     id: 'https://soundcloud.com/raxxo-1/ng-mare',
                     type: 'soundcloud',
                     disableTiming: false }).to.deep.equal(entity);
+                done();
+            }).catch(reason => {
+                throw new Error(reason);
+            });
+        });
+
+
+        it('should return playlist info', (done) => {
+            soundcloud.info('https://soundcloud.com/dawn-records/sets/killove-fireproof').then(entity => {
+                entity.meta = [];
+                expect({ duration: 786,
+                    title: 'KILLOVE FIREPROOF!',
+                    thumbnail: 'https://i1.sndcdn.com/artworks-000199813485-7m1i1f-large.jpg',
+                    url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                    id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                    type: 'soundcloudList',
+                    disableTiming: false,
+                    meta: [] }).to.deep.equal(entity);
+                done();
+            }).catch(reason => {
+                throw new Error(reason);
+            });
+        });
+    });
+
+    describe('entities', () => {
+        it('should return track entity', () => {
+            soundcloud.entities('https://soundcloud.com/raxxo-1/ng-mare').then(entity => {
+                expect({ duration: 98,
+                    title: 'NG Mare',
+                    thumbnail: 'https://i1.sndcdn.com/artworks-000145582566-ihitj3-large.jpg',
+                    url: 'https://soundcloud.com/raxxo-1/ng-mare',
+                    id: 'https://soundcloud.com/raxxo-1/ng-mare',
+                    type: 'soundcloud',
+                    disableTiming: false }).to.deep.equal(entity);
+                done();
+            }).catch(reason => {
+                throw new Error(reason);
+            });
+        });
+
+        it('should return playlist entities', (done) => {
+            soundcloud.entities('https://soundcloud.com/dawn-records/sets/killove-fireproof').then(entity => {
+                expect([ { duration: 137.26,
+                    title: 'KilLove Fireproof!',
+                    thumbnail: null,
+                    url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                    id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                    type: 'soundcloud',
+                    disableTiming: false },
+                    { duration: 82.71,
+                        title: 'EmotionaRhythm',
+                        thumbnail: null,
+                        url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        type: 'soundcloud',
+                        disableTiming: false },
+                    { duration: 84.278,
+                        title: 'DANGO ON  FIRE',
+                        thumbnail: null,
+                        url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        type: 'soundcloud',
+                        disableTiming: false },
+                    { duration: 99.875,
+                        title: '夜明けのウェアウルフ',
+                        thumbnail: null,
+                        url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        type: 'soundcloud',
+                        disableTiming: false },
+                    { duration: 120.121,
+                        title: '死んだバラと悪魔のはなし',
+                        thumbnail: null,
+                        url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        type: 'soundcloud',
+                        disableTiming: false },
+                    { duration: 81.169,
+                        title: 'SILVER STORM',
+                        thumbnail: null,
+                        url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        type: 'soundcloud',
+                        disableTiming: false },
+                    { duration: 95.199,
+                        title: 'ライト・グラフィティ',
+                        thumbnail: null,
+                        url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        type: 'soundcloud',
+                        disableTiming: false },
+                    { duration: 85.846,
+                        title: '雨の日に',
+                        thumbnail: null,
+                        url: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        id: 'https://soundcloud.com/dawn-records/sets/killove-fireproof',
+                        type: 'soundcloud',
+                        disableTiming: false } ]).to.deep.equal(entity);
                 done();
             }).catch(reason => {
                 throw new Error(reason);
