3c10894d9dcf7a64a7d57deb1a0ae9bf2395a7e3
kotborealis <kotborealis@gmail.com>
2017-07-07 00:54:04 +0300

    Fix case

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {};
 
-const capitalize = ([first,...rest]) => first.toUpperCase() + rest.join('').toLowerCase();
+const capitalize = ([first,...rest]) => first.toUpperCase() + rest.join('');
 
 ['coub', 'file', 'twitchStream', 'vimeo', 'youtube', 'youtubeList', 'soundcloud'].forEach(provider => {
     module.exports[provider] = require('./lib/providers/Provider' + capitalize(provider));
